40c0167b138e56aa97e79fbdce33574c507b67d2
ArkadiiRaih <ArkadioRaih@yandex.ru>
2020-05-04 03:54:26 +1000

    Updates

---
+++ precache-manifest.4d1a8fe752f452a0e4be646c08501baf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "859ca456fdfa266df60448367a13e0ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2689c8e71bc4c8680274",
+    "url": "/static/css/main.8c14b4b5.chunk.css"
+  },
+  {
+    "revision": "606d8d4511d89ad6f21b",
+    "url": "/static/js/2.21018794.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.21018794.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2689c8e71bc4c8680274",
+    "url": "/static/js/main.6852664e.chunk.js"
+  },
+  {
+    "revision": "92d99368058e735eb2bb",
+    "url": "/static/js/runtime-main.2ec78e9d.js"
+  }
+]);
