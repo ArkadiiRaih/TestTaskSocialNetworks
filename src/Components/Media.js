85eed643324fb82e3526f37c9684c0a43006c887
ArkadiiRaih <ArkadioRaih@yandex.ru>
2020-05-04 14:08:19 +1000

    Add check urls

--- src/Components/Media.js
+++ src/Components/Media.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const bgs = [
   "./backgrounds/bg1.png",
@@ -6,16 +6,37 @@ const bgs = [
   "./backgrounds/bg3.png",
 ];
 
-function Media({ img, video, id }) {
+function Media({ img, video, id, setWithMedia }) {
+  const [usedResource, setUsedResource] = useState(video || img || bgs[id % 3]);
+
+  useEffect(() => {
+    if (img) {
+      fetch(img, {
+        method: "HEAD",
+      }).then(
+        (res) => {
+          if (res.status == 404) {
+            setUsedResource(bgs[id % 3]);
+            setWithMedia(false);
+          }
+        },
+        (reason) => {
+          setUsedResource(bgs[id % 3]);
+          setWithMedia(false);
+        }
+      );
+    }
+  }, [img, id]);
   if (!video) {
     return (
       <div
         className="slide--background"
         style={{
-          background: `url(${img || bgs[id % 3]}) center center / cover`,
+          background: `url(${usedResource}) center center / cover`,
+          // background: `url(./daskdmasfmsaf.png) center center / cover`,
         }}
-        onError={(error) => {
-          console.log(error);
+        onError={() => {
+          console.log(123);
         }}
       ></div>
     );
--- src/Components/Slide.js
+++ src/Components/Slide.js
@@ -1,80 +1,24 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import Media from "./Media";
 import Icon from "./Icon";
-
-function getFontSize(text) {
-  if (!text) return { fontSize: "40px", br: 0 };
-  const length = text.length;
-  let fontSize = 0;
-  let br = 0;
-  if (length < 120) {
-    fontSize = 80;
-    br = 15;
-  } else if (length < 128) {
-    fontSize = 75;
-    br = 15;
-  } else if (length < 136) {
-    fontSize = 70;
-    br = 17;
-  } else if (length < 152) {
-    fontSize = 65;
-    br = 19;
-  } else {
-    fontSize = 60;
-    br = 20;
-  }
-
-  fontSize = window.innerWidth < 1921 ? `${fontSize / 2}px` : `${fontSize}px`;
-  return { fontSize, br };
-}
-
-function getLines(text, br) {
-  if (!text) return [];
-  const words = text.split(" ");
-  let res = "";
-  let len = 0;
-  let lines = [];
-
-  for (let i = 0; i < words.length - 1; i++) {
-    let word = words[i];
-    len += word.length;
-    if (len + words[i + 1].length < br) {
-      res += word + " ";
-    } else {
-      res += word;
-      lines.push(res);
-      res = "";
-      len = 0;
-    }
-    if (lines.length == 8) {
-      lines[7] = lines[7].trimEnd().concat("...");
-      break;
-    }
-    if (i == words.length - 2) {
-      res += words[i + 1];
-      lines.push(res);
-    }
-  }
-  return lines;
-}
+import { getLines, getFontSize } from "../utils";
 
 function Slide({ id, img, video, text, author, network }) {
-  const withMedia = img || video;
+  const [withMedia, setWithMedia] = useState(img || video);
   const { fontSize, br } = getFontSize(text);
   const lines = getLines(text, br);
-  console.log(lines);
 
   return (
     <div className="carousel--item" style={{ position: "relative" }}>
       <div className={`slide ${withMedia ? "slide-media" : "slide-open"}`}>
-        <Media img={img} video={video} id={id} />
+        <Media setWithMedia={setWithMedia} img={img} video={video} id={id} />
         <div className="slide--content">
           <p className="slide--text" style={{ fontSize: fontSize }}>
             {lines.map((line) => (
-              <>
+              <span key={line}>
                 {line}
                 <br />
-              </>
+              </span>
             ))}
           </p>
           <p className="slide--author">
